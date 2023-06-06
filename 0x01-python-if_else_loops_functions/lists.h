4be34b605cf78051a0ad53da8bac983fdc9b78d8
kemboi-73 <kemboimichael52@gmail.com>
2023-06-06 18:11:17 +0000

    a function in C that inserts a number into a sorted singly linked list

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -5,10 +6,10 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
- * for Holberton project
+ *
  */
-
 typedef struct listint_s
 {
     int n;
