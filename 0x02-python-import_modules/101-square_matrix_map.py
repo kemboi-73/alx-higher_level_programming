3a0a329b90276dd3049a747495d73277a27d48ae
kemboi-73 <kemboimichael52@gmail.com>
2023-06-14 20:03:30 +0000

    a function that computes the square value of all integers of a matrix using map

--- 0x02-python-import_modules/101-square_matrix_map.py
+++ 0x02-python-import_modules/101-square_matrix_map.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    add = __import__('magic_calculation_102').add
-    sub = __import__('magic_calculation_102').sub
-
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 7):
-            c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
+def square_matrix_map(matrix=[]):
+    return (list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix)))
