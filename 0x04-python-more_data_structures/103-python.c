a2bd174371bf612b538c44aa084d415253526fea
kemboi-73 <kemboimichael52@gmail.com>
2023-06-16 13:44:18 +0000

    C functions that print some basic info about Python lists and Python bytes objects.

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,56 +1,49 @@
 #include <Python.h>
-#include <stdio.h>
 
-void print_python_list(PyObject *p);
-void print_python_bytes(PyObject *p);
-
-/**
- * print_python_list - Prints information about a Python list object
- * @p: Pointer to the PyObject representing the list
- */
 void print_python_list(PyObject *p)
 {
 	Py_ssize_t size, i;
 	PyObject *item;
 
 	size = PyList_Size(p);
+
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
 
 	for (i = 0; i < size; i++)
 	{
-		item = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-		if (PyBytes_Check(item))
-			print_python_bytes(item);
+		item = PyList_GET_ITEM(p, i);
+		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
 	}
 }
 
-/**
- * print_python_bytes - Prints information about a Python bytes object
- * @p: Pointer to the PyObject representing the bytes object
- */
 void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t size, i;
-	char *str;
+	PyBytesObject *bytes;
+	unsigned char byte;
 
-	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
+		printf("[.] bytes object info\n");
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
-	size = PyBytes_Size(p);
-	str = PyBytes_AsString(p);
+	bytes = (PyBytesObject *)p;
+	size = PyBytes_GET_SIZE(p);
 
-	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", str);
-	printf("  first %ld bytes:", size + 1 > 10 ? 10 : size + 1);
+	printf("[.] bytes object info\n");
+	printf("  size: %zd\n", size);
+	printf("  trying string: %s\n", bytes->ob_sval);
+
+	printf("  first %zd bytes:", size + 1 < 10 ? size + 1 : 10);
 	for (i = 0; i < size + 1 && i < 10; i++)
-		printf(" %02hhx", str[i]);
+	{
+		byte = bytes->ob_sval[i];
+		printf(" %02x", byte);
+	}
 	printf("\n");
 }
 
