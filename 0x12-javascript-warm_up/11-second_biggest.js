ea4935648d30d209dcf7eecff75962eea55efe98
kemboi-73 <kemboimichael52@gmail.com>
2023-09-11 19:48:08 +0000

    a script that searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,30 +1,12 @@
 #!/usr/bin/node
 
-const process = require('process');
-const myArray = (process.argv);
-const count = process.argv.length - 2;
+const args = process.argv.slice(2).map(Number);
 
-function findSecondLargest (array) {
-  if (array.length < 4) {
-    return 0;
-  }
-  let largest = array[2];
-  let secondLargest = array[2];
-  for (let i = 3; i < array.length; i++) {
-    if (array[i] > largest) {
-      secondLargest = largest;
-      largest = array[i];
-    } else if (array[i] > secondLargest && array[i] !== largest) {
-      secondLargest = array[i];
-    }
-  }
-  return secondLargest;
-}
-
-if (count === 0) {
-  console.log('0');
-} else if (count === 1) {
-  console.log('0');
+if (args.length <= 1) {
+  console.log(0);
 } else {
-  console.log(findSecondLargest(myArray));
+  args.sort((a, b) => b - a); // Sort the numbers in descending order
+  const secondLargest = args[1]; // Get the second element (the second largest)
+  console.log(secondLargest);
 }
+
