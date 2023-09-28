54679990759cc0070e762729a0a5dc0ea8c40920
kemboi-73 <kemboimichael52@gmail.com>
2023-09-28 13:36:21 -0700

    a function that finds a peak in a list of unsorted integers.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-"""contains the function find_peak"""
+"""Find the biggest number in a list with lowest search complexity"""
 
 
 def find_peak(list_of_integers):
-    """finds a peak"""
-    li = list_of_integers
-    l = len(li)
-    if l == 0:
-        return
-    m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
-        return li[m]
-    if m != l - 1 and li[m + 1] > li[m]:
-        return find_peak(li[m + 1:])
-    return find_peak(li[:m])
+    """Find the biggest number with the a short time complexity"""
+
+    # Checks if list is empty
+    if list_of_integers == []:
+        return None
+
+    list_size = len(list_of_integers)
+    if list_size == 1:
+        return list_of_integers[0]
+    if list_size == 2:
+        return max(list_of_integers)
+
+    # get the middle element and assign peak
+    mid = int(list_size / 2)
+    peak = list_of_integers[mid]
+
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
